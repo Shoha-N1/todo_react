907e36d00181183ad0a0fa1401caee1c2f135635
Shoha-N1 <abduvohidovs85@gmail.com>
2023-03-03 15:11:03 +0500

    Commit

--- new_todo/src/components/todo/Todo.jsx
+++ new_todo/src/components/todo/Todo.jsx
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import Item from "./item/Item";
 
 export default function Todo() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos"))
+      ? JSON.parse(localStorage.getItem("todos"))
+      : []
+  );
+
+  localStorage.setItem(
+    "todos",
+    JSON.stringify(todos) ? JSON.stringify(todos) : "[]"
+  );
+
   const [todoName, setTodoName] = useState("");
 
   function handleSubmit(evt) {
@@ -10,8 +20,14 @@ export default function Todo() {
 
     setTodos([
       ...todos,
-      { name: todoName, isDone: false, id: todos.length + 1, dueDate: new Date(Date.now()) },
+      {
+        name: todoName,
+        isDone: false,
+        id: todos.length + 1,
+        dueDate: new Date(Date.now()),
+      },
     ]);
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   function handleToggle(id) {
@@ -19,49 +35,55 @@ export default function Todo() {
     let index = tempTodos.findIndex((todo) => todo.id === id);
 
     if (typeof index !== "number") return;
-
     tempTodos[index].isDone = !tempTodos[index].isDone;
 
     setTodos(tempTodos);
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   function handleDelete(id) {
     setTodos(todos.filter((todo) => todo.id !== id));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   function handleClearAll(id) {
-    setTodos([])
+    setTodos([]);
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
-//   function handleChecked(id) {
-    
-//   }
+  //   function handleChecked(id) {
 
+  //   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input className="inp-value"
-          value={todoName}
-          onChange={(evt) => setTodoName(evt.target.value)}
-          type="text"
-        />
-        <button type="submit">Add</button>
-        <button onClick={() => handleClearAll(todos)} type="button">ClearAll</button>
-      </form>
-      <div>
-        {todos.map((todo) => (
-          <Item
-            onDelete={() => handleDelete(todo.id)}
-            key={todo.id}
-            id={todo.id}
-            onToggle={() => handleToggle(todo.id)}
-            // onChecked={() => }
-            name={todo.name}
-            isDone={todo.isDone}
-            dueDate={todo.dueDate}
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="inp-value"
+            value={todoName}
+            onChange={(evt) => setTodoName(evt.target.value)}
+            type="text"
           />
-        ))}
+          <button type="submit">Add</button>
+          <button onClick={() => handleClearAll(todos)} type="button">
+            ClearAll
+          </button>
+        </form>
+        <div>
+          {todos.map((todo) => (
+            <Item
+              onDelete={() => handleDelete(todo.id)}
+              key={todo.id}
+              id={todo.id}
+              onToggle={() => handleToggle(todo.id)}
+              // onChecked={() => }
+              name={todo.name}
+              isDone={todo.isDone}
+              dueDate={todo.dueDate}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- new_todo/src/components/todo/item/Item.jsx
+++ new_todo/src/components/todo/item/Item.jsx
@@ -7,13 +7,11 @@ export default function Item({name, isDone, onToggle, id, onDelete, onChecked, d
 
   return (
     <div>
-         <label htmlFor="">
-        <input onChange={onToggle} onClick={onChecked} checked={isDone} type="checkbox" className='inp' />
-       
+        <input onChange={onToggle}  onClick={onChecked} checked={isDone} type="checkbox" className='inp' />
+       <label htmlFor=""></label>
         <span>{name}</span>
         <span className='date-class' >{`${newDueDate.toLocaleDateString()} ${newDueDate.toLocaleTimeString()}`}</span>
         <button onClick={onDelete}>x</button>
-        </label>    
     </div>
   )
 }
